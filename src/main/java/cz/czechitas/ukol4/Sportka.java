52dd25449298eb57ab598bb95bc968f8d21c7130
Lenka Altmanova <lenkaaltmanova989@gmail.com>
2023-03-23 19:54:30 +0100

    4. úkol finální verze

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -8,40 +8,29 @@ import java.util.List;
  * Třída pro losování Sportky.
  */
 public class Sportka {
-  private final List<Integer> osudi = new ArrayList<>();
+    private final List<Integer> osudi = new ArrayList<>();
 
-  public Sportka() {
-    //TODO naplnit osudí čísly 1 až 49.
-  }
+    public Sportka() {
+        for (int i = 1; i < 50; i++) {
+            osudi.add(i);
+            //System.out.println(osudi);
 
-  /**
-   * Zamíchá osudí.
-   *
-   * @see Collections#shuffle(List)
-   */
-  public void zamichat() {
-    //TODO zamíchat osudí
-  }
+        }
+    }
 
-  /**
-   * Vrátí seznam s 6 vylosovanými čísly.
-   *
-   * @return Seznam vylosovaných čísel.
-   *
-   * @see List#subList(int, int)
-   */
-  public List<Integer> vylosovanaCisla() {
-    //TODO Vrátit seznam prvních 6 čísel z osudí.
-    return null;
-  }
+    public void zamichat() {
 
-  /**
-   * Vrátí sedmé vylosované číslo (dodatkové číslo)
-   *
-   * @return Dodatkové číslo.
-   */
-  public Integer dodatkoveCislo() {
-    //TODO Vrátit sedmé číslo z osudí.
-    return null;
-  }
+        Collections.shuffle(osudi);
+
+    }
+
+    public List<Integer> vylosovanaCisla() {
+        List<Integer> top6 = new ArrayList<Integer>(osudi.subList(Math.max(osudi.size() - 6, 0), osudi.size()));
+        return top6;
+    }
+
+    public Integer dodatkoveCislo() {
+        return osudi.get(6);
+
+    }
 }
